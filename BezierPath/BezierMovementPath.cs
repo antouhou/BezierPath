d1c564618566af95efb1d783643e3e74b10c6c0a
Anton Suprunchuk <anton.suprunchuk@gmail.com>
2021-09-19 19:47:48 +0300

    feat: optimize memory usage

--- BezierPath/BezierMovementPath.cs
+++ BezierPath/BezierMovementPath.cs
@@ -4,63 +4,98 @@ using UnityEngine;
 
 namespace BezierPath
 {
+    internal struct CurveEntry
+    {
+        public double StartDistance;
+        public double EndDistance;
+        public CubicBezierCurve Curve;
+    }
     public class BezierMovementPath
     {
-        public double Length;
-        public BezierPathSegment[] PathSegments;
-        public List<Vector3> Points = new List<Vector3>();
-        public double SegmentSize;
+        private double _estimatedLength = 0;
+        private readonly List<CurveEntry> _curves = new List<CurveEntry>();
+        private readonly List<Vector3> _controlPointsList = new List<Vector3>();
+        private float _smoothness = 0;
 
-        public BezierMovementPath(IReadOnlyList<Vector3> points, float smoothness, float segmentSize)
+        public BezierMovementPath(List<Vector3> controlPoints, float smoothness)
         {
-            PathSegments = points.Select((center, index) =>
-            {
-                // For the first tile on the path enter point is the center of the tile
-                var previousCenter = index == 0 ? center : points[index - 1];
-                // For the last tile exit is the center of the tile
-                var nextCenter = index == points.Count - 1 ? center : points[index + 1];
-
-                var enterPoint = Vector3.Lerp(previousCenter, center, 0.5f);
-                var exitPoint = Vector3.Lerp(center, nextCenter, 0.5f);
-
-                var enterTangent = Vector3.Lerp(enterPoint, center, smoothness);
-                var exitTangent = Vector3.Lerp(exitPoint, center, smoothness);
-
-                var curve = new CubicBezierCurve(
-                    enterPoint,
-                    exitPoint,
-                    enterTangent,
-                    exitTangent
-                );
-
-                return new BezierPathSegment(curve, segmentSize);
-            }).ToArray();
-
-            for (var i = 0; i < PathSegments.Length; i++)
-            {
-                var segment = PathSegments[i];
-                // For all the segments except the first, the last point of the previous segment
-                // is approximately equal to starting point of the next
-                Points.AddRange(i == 0 ? segment.Points : segment.Points.Skip(1));
-
-                SegmentSize = SegmentSize == 0 ? segment.SegmentSize : (SegmentSize + segment.SegmentSize) / 2;
-            }
-
-            Length = Points.Count * SegmentSize;
+            Rebuild(controlPoints, smoothness);
         }
 
-        public Vector3 Move(float distance)
+        public void Clear()
         {
-            if (distance > Length) return Points.Last();
+            _controlPointsList.Clear();
+            _curves.Clear();
+            _estimatedLength = 0.0;
+            _smoothness = 0;
+        }
+        
+        public BezierMovementPath() {}
 
-            var previousClosestPointIndex = (int) (distance / SegmentSize);
-            var nextClosestPointIndex = (int) (distance / SegmentSize) + 1;
+        public void Rebuild(List<Vector3> controlPoints, float smoothness)
+        {
+            Clear();
 
-            var previousClosestPoint = Points[previousClosestPointIndex];
-            var nextClosestPoint = Points[nextClosestPointIndex];
-            var overshot = distance - (distance - SegmentSize * previousClosestPointIndex);
+            _smoothness = smoothness;
+            _controlPointsList.AddRange(controlPoints);
+            for (var i = 0; i < controlPoints.Count; i++)
+            {
+                _curves.Add(CreateCurveEntry(controlPoints[i], i));
+            }
+        }
 
-            return Vector3.MoveTowards(previousClosestPoint, nextClosestPoint, (float) overshot);
+        private CurveEntry CreateCurveEntry(Vector3 center, int index)
+        {
+            var previousCenter = index == 0 ? center : _controlPointsList[index - 1];
+            // For the last tile exit is the center of the tile
+            var nextCenter = index == _controlPointsList.Count - 1 ? center : _controlPointsList[index + 1];
+
+            var enterPoint = Vector3.Lerp(previousCenter, center, 0.5f);
+            var exitPoint = Vector3.Lerp(center, nextCenter, 0.5f);
+
+            var enterTangent = Vector3.Lerp(enterPoint, center, _smoothness);
+            var exitTangent = Vector3.Lerp(exitPoint, center, _smoothness);
+
+            var curve = new CubicBezierCurve(
+                enterPoint,
+                exitPoint,
+                enterTangent,
+                exitTangent
+            );
+
+            var newEstimatedLength = _estimatedLength + curve.estimatedLength;
+            var curveEntry = new CurveEntry
+            {
+                Curve = curve,
+                StartDistance = _estimatedLength,
+                EndDistance = newEstimatedLength,
+            };
+
+            _estimatedLength = newEstimatedLength;
+            return curveEntry;
+        }
+
+        private CurveEntry GetCurveAtDistance(float distance)
+        {
+            return _curves.FirstOrDefault(curveIndex => distance >= curveIndex.StartDistance && distance <= curveIndex.EndDistance);
+        }
+
+        public Vector3 GetPointAt(float distance)
+        {
+            if (distance > _estimatedLength) return GetLastPoint();
+
+            var curve = GetCurveAtDistance(distance);
+            return curve.Curve.GetPoint(curve.Curve.NormalizeMovement(distance - curve.StartDistance));
+        }
+
+        public Vector3 GetLastPoint()
+        {
+            return _curves.Last().Curve.endPoint;
+        }
+
+        public double GetEstimatedLength()
+        {
+            return _estimatedLength;
         }
     }
 }
